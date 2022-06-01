21ef98b0c6b6202a7ff0a736b640d21956f8b7b7
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-06-01 19:52:14 +0530

    Added code for employee attendance

---
+++ EmployeeWage.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.Day4;
+
+import java.util.WeakHashMap;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+            System.out.println("Welcome to Employee Wage Computation Program ");
+            int employee = (int) ((Math.random() * 10) % 2);
+            System.out.println(employee);
+            if (employee == 1) {
+                System.out.println("Employee is present");
+            } else {
+                System.out.println("Employee is absent");
+            }
+    }
+    }
+

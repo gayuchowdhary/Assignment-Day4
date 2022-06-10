c06795a01b850a9c6b7322b8e68ae70f20ae8679
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-06-10 09:48:30 +0530

    Refactored the code

---
+++ EmpWageCompute.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.Day4;
+
+public class EmpWageCompute {
+    public static int calculateWage(int totalWorkingHour, int totalWorkingDay, int empWagePerHour) {
+        int empHour;
+        while (totalWorkingHour <= 100 && totalWorkingDay < 20) {
+            totalWorkingDay++;
+            int empCheck = (int) (Math.random() * 10) % 3;
+            switch (empCheck) {
+                case 1:
+                    empHour = 8;
+                    break;
+                case 2:
+                    empHour = 4;
+                    break;
+                default:
+                    empHour = 0;
+            }
+
+            totalWorkingHour += empHour;
+            System.out.println("Day: " + totalWorkingDay + " Working Hour: " + totalWorkingHour);
+        }
+
+        int totalWage = (totalWorkingHour * empWagePerHour);
+        return totalWage;
+    }
+
+    public static void main(String[] args) {
+        int totalWorkingHour = 0, totalWorkingDay = 0;
+        int empWagePerHour = 20, totalWage;
+        totalWage = EmpWageCompute.calculateWage(totalWorkingHour, totalWorkingDay, empWagePerHour);
+        System.out.println("Total Employee Wage: " + totalWage);
+    }
+}
+
